3dc6e70efa9513495b82f967cce07b546166bbc2
unknown <ozhan728@gmail.com>
2024-06-04 02:18:56 +0330

    added user form

---
+++ accounts/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from .models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+
+class UserCreationForm(forms.ModelForm):
+
+    password1 = forms.CharField(label='Password',widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password',widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email','phone_number','full_name']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
+            raise ValidationError('Passwords Do Not Match')
+        return cd['password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField(help_text='You can not change password using <a href=\"../password/\" > this form </a>')
+
+    class Meta:
+        model = User
+        fields = ['email','phone_number','full_name','password','last_login']
+
+
+
+
