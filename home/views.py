4cdf055a6e088b3aa18ce7069d477bfd38a8e766
unknown <ozhan728@gmail.com>
2024-07-21 18:04:28 +0330

    added categories

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , get_object_or_404
 from django.views import View
-from .models import Product
+from .models import Product,Category
 import time
 from Shop.celery_conf import celery_app
 # Create your views here.
@@ -14,11 +14,15 @@ from Shop.celery_conf import celery_app
 #         print(myFile)
 
 class HomeView(View):
-    def get(self,request):
+    def get(self,request,category_slug=None):
         # this is for celery test only
         # my_task.delay()
         products = Product.objects.filter(available=True)
-        return render(request,'home/home.html',{'products':products})
+        categories = Category.objects.all()
+        if category_slug:
+            category = Category.objects.get(slug=category_slug)
+            products = products.filter(category=category)
+        return render(request,'home/home.html',{'products':products,'categories':categories})
 
 
 class ProductDetailView(View):
